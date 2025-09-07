9f902c204340e95ee6630cd0c42ef5b951e0093e
KaEde-ki <markaxel922@gmail.com>
2025-09-07 21:22:23 +0800

    Add .gitignore to ignore Git folder and build files

---
+++ homework1/Homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence: ");
+            String str = Console.ReadLine().ToLower();
+
+            str = str.Replace(" ", "");
+
+            char[] wordArray = str.ToCharArray();
+            Array.Reverse(wordArray);
+            string reversed = new string(wordArray);
+
+            if(str == reversed)
+            {
+                Console.WriteLine("This is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("This is not a palindrome.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- homework1/Homework1/Properties/AssemblyInfo.cs
+++ homework1/Homework1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("Homework1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("Homework1")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38b600a2-3db8-4348-a9c3-395cb11ca6ba")]
+[assembly: Guid("e6fe5fdc-4565-489a-b50c-c98baeb4cd68")]
 
 // Version information for an assembly consists of the following four values:
 //
