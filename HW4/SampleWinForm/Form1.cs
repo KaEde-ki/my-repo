d308be9b969c279c519f297b615489259a3b412a
KaEde-ki <markaxel922@gmail.com>
2025-10-18 19:01:20 +0800

    added HW4

---
+++ HW4/SampleWinForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleWinForm
+{
+    public partial class Form1 : Form
+    {
+        string imagePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePickerBirthday.Format = DateTimePickerFormat.Custom;
+            dateTimePickerBirthday.CustomFormat = "yyyy-MM-dd";
+        }
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string firstName = textBoxFirst.Text;
+            string lastName = textBoxLast.Text;
+            string mobile = textBoxMobile.Text;
+
+            string gender = "";
+
+            if (radioButtonMale.Checked)
+                gender = "Male";
+            else if (radioButtonFemale.Checked)
+                gender = "Female";
+
+            string status = comboStatus.Text;
+            DateTime birthday = dateTimePickerBirthday.Value;
+            string location = textBoxLocation.Text;
+
+            Form2 f2 = new Form2(firstName, lastName, mobile, gender, status, birthday, location, imagePath);
+            f2.Show();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                imagePath = ofd.FileName;
+                pictureBox1.Image = Image.FromFile(imagePath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                buttonBrowse.Hide();
+            }
+        }
+    }
+}
---
+++ HW4/SampleWinForm/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleWinForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string firstName, string lastName, string mobile, string sex, string status, DateTime birthday, string location, string imagePath)
+        {
+            InitializeComponent();
+
+            labelFirst.Text = "First Name: " + firstName;
+            labelLast.Text = "Last Name: " + lastName;
+            labelMobile.Text = "Mobile: " + mobile;
+            labelSex.Text = "Sex: " + sex;
+            labelStatus.Text = "Status: " + status;
+            labelBirthday.Text = "Birthday: " + birthday.ToString("yyyy-MM-dd");
+            labelLocation.Text = "Location: " + location;
+
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                pictureBoxDisplay.Image = Image.FromFile(imagePath);
+                pictureBoxDisplay.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+    }
+}
